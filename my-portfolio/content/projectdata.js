3d4353ba2c8aeaddb69629758eedf03d21988097
Kush-munot <munot.kgm@gmail.com>
2022-10-27 20:06:24 +0530

    images me error hai projectdata.js

---
+++ my-portfolio/content/projectdata.js
@@ -0,0 +1,31 @@
+import passwordChecker from '/public/assets/password.png'
+import Imagizer from '/public/assets/imagizer.png'
+
+
+
+module.exports = [
+    {
+        id: 1,
+        name: "Unlock - Password Strength Checker",
+        coverURL: passwordChecker,
+        description:
+            "Created a 100% safe Password Strength Checker that asks you for data related to Password but not the Password itself. I have used the Password Entropy Concept to Check the Strength of the Password.",
+        githubURL: "https://github.com/Kush-munot/Password-Strength-Checker",
+        previewURL: "https://kmunlock.netlify.app/",
+        tools: ["React.js", "Material UI", "Notion", "Markdown", "Github"],
+        pinned: true,
+    },
+    {
+        id: 2,
+        name: "Python Image Package",
+        coverURL: Imagizer,
+        description:
+            "Created a 100% safe Password Strength Checker that asks you for data related to Password but not the Password itself. I have used the Password Entropy Concept to Check the Strength of the Password.",
+        githubURL: "https://github.com/Kush-munot/Password-Strength-Checker",
+        previewURL: "https://kmunlock.netlify.app/",
+        tools: ["React.js", "Material UI", "Notion", "Markdown", "Github"],
+        pinned: true,
+    },
+
+    /* ..............Other Projects......... */
+];
