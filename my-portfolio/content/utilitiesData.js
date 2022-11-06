e7cbf4477c624e48224196bfb0c45ceff299de9d
Kush-munot <munot.kgm@gmail.com>
2022-11-06 15:32:38 +0530

    Utilites Page Done

---
+++ my-portfolio/content/utilitiesData.js
@@ -0,0 +1,297 @@
+import {
+    SiVisualstudiocode,
+    SiMicrosoftedge,
+    SiWindowsterminal,
+    SiSamsung,
+    SiGooglechrome,
+    SiReact,
+    SiNextdotjs,
+    SiNetlify,
+    SiMaterialui,
+    SiGit,
+    SiMongodb,
+    SiBootstrap,
+    SiJavascript,
+    SiC,
+    SiCplusplus,
+    SiJava,
+    SiPython,
+    SiNodedotjs,
+    SiExpress,
+    SiHtml5,
+    SiCss3,
+    SiGeeksforgeeks,
+    SiFigma,
+    SiSpotify,
+    SiObsstudio,
+    SiGrammarly,
+    SiNotion,
+    SiCodechef,
+    SiCodeforces,
+    SiHackerearth,
+    SiHackerrank,
+    SiLeetcode,
+    SiGithub,
+} from "react-icons/si";
+import {
+    BsFillPaletteFill,
+    BsWindows,
+} from "react-icons/bs";
+import { FaGitAlt, FaSearch } from "react-icons/fa";
+
+/* import SVG from "@components/SVG"; //==> not created yet */
+
+const utilities = {
+    title: "Utilities",
+    description:
+        "In case you are wondering What tech I use, Here's the list of what tech I'm currently using for coding on the daily basis. This list is always changing.",
+    lastUpdate: "November 06, 2022",
+
+    /* System */
+
+    system: {
+        title: "System",
+        data: [
+            {
+                name: "VSCode",
+                description: "Primary Code editor",
+                Icon: SiVisualstudiocode,
+                link: "https://code.visualstudio.com/download",
+            },
+            {
+                name: "Infinity Ddark Theme",
+                description: "VS Code theme",
+                Icon: BsFillPaletteFill,
+                link: "https://marketplace.visualstudio.com/items?itemName=nadim-vscode.infinity-dark-theme",
+            },
+            {
+                name: "Microsoft Edge",
+                description: "Default Browser",
+                Icon: SiMicrosoftedge,
+                link: "https://www.microsoft.com/en-us/edge?form=MA13FJ",
+            },
+            {
+                name: "Google Chrome",
+                description: "Internet Browser",
+                Icon: SiGooglechrome,
+                link: "https://www.google.com/chrome/",
+            },
+            {
+                name: "Windows 11",
+                description: "Windows Version",
+                Icon: BsWindows,
+                link: "https://www.google.com/chrome/",
+            },
+            {
+                name: "Command Prompt",
+                description: "Windows Terminal",
+                Icon: SiWindowsterminal,
+                link: "https://www.google.com/chrome/",
+            },
+            {
+                name: "Samsung",
+                description: "Phone Brand",
+                Icon: SiSamsung,
+                link: "",
+            },
+
+            /* .............Other........ */
+        ],
+    },
+
+    /* Coding Tools */
+    tools: {
+        title: "Coding Tools",
+        data: [
+            {
+                name: "React.js",
+                description: "Primary Front-end library",
+                Icon: SiReact,
+                link: "https://reactjs.org/",
+            },
+            {
+                name: "Next.js",
+                description: "Primary Web Development Framework",
+                Icon: SiNextdotjs,
+                link: "https://nextjs.org/",
+            },
+            {
+                name: "Material UI",
+                description: "Primary Web Development Framework",
+                Icon: SiMaterialui,
+                link: "",
+            },
+            {
+                name: "Javascript",
+                description: "Primary Web Development Framework",
+                Icon: SiJavascript,
+                link: "",
+            },
+            {
+                name: "Node.js",
+                description: "Primary Web Development Framework",
+                Icon: SiNodedotjs,
+                link: "",
+            },
+            {
+                name: "Mongo DB",
+                description: "Primary Web Development Framework",
+                Icon: SiMongodb,
+                link: "",
+            },
+            {
+                name: "Express.js",
+                description: "Primary Web Development Framework",
+                Icon: SiExpress,
+                link: "",
+            },
+            {
+                name: "CSS",
+                description: "Primary Web Development Framework",
+                Icon: SiCss3,
+                link: "",
+            },
+            {
+                name: "HTML",
+                description: "Primary Web Development Framework",
+                Icon: SiHtml5,
+                link: "",
+            },
+            {
+                name: "Bootstrap",
+                description: "Primary Web Development Framework",
+                Icon: SiBootstrap,
+                link: "",
+            },
+            {
+                name: "C++",
+                description: "Primary Web Development Framework",
+                Icon: SiCplusplus,
+                link: "",
+            },
+            {
+                name: "C",
+                description: "Coding Language",
+                Icon: SiC,
+                link: "",
+            },
+            {
+                name: "Java",
+                description: "Coding Language",
+                Icon: SiJava,
+                link: "",
+            },
+            {
+                name: "Python",
+                description: "Coding Language",
+                Icon: SiPython,
+                link: "",
+            },
+            /* .............Other........ */
+        ],
+    },
+
+    /* Software/Applications */
+
+    software: {
+        title: "Software/Applications",
+        data: [
+            {
+                name: "Figma",
+                description: "UI And Graphics",
+                Icon: SiFigma,
+                link: "",
+            },
+            {
+                name: "Notion",
+                description: "Notebook",
+                Icon: SiNotion,
+                link: "",
+            },
+            {
+                name: "Spotify",
+                description: "Music",
+                Icon: SiSpotify,
+                link: "",
+            },
+            {
+                name: "OBS Studio",
+                description: "Recording",
+                Icon: SiObsstudio,
+                link: "",
+            },
+            {
+                name: "Grammarly",
+                description: "Recording",
+                Icon: SiGrammarly,
+                link: "",
+            },
+            {
+                name: "Netlify",
+                description: "Deploying Applications",
+                Icon: SiNetlify,
+                link: "",
+            },
+            {
+                name: "Git",
+                description: "Version Control",
+                Icon: SiGit,
+                link: "",
+            },
+            {
+                name: "Github Desktop",
+                description: "Open Source",
+                Icon: SiGithub,
+                link: "",
+            },
+            /* .............Other......... */
+        ],
+    },
+
+    /* Coding Platforms */
+
+    coding: {
+        title: "Coding Platform Profiles",
+        data: [
+            {
+                name: "Codechef",
+                description: "Competitve Programming",
+                Icon: SiCodechef,
+                link: "https://www.codechef.com/users/kushmunot",
+            },
+            {
+                name: "Codeforces",
+                description: "Competitive Programming",
+                Icon: SiCodeforces,
+                link: "https://codeforces.com/profile/axelblaze_17",
+            },
+            {
+                name: "HackerRank",
+                description: "Competitve Programming",
+                Icon: SiHackerrank,
+                link: "https://www.hackerrank.com/Kush_Munot",
+            },
+            {
+                name: "Geeks For Geeks",
+                description: "DSA Programming",
+                Icon: SiGeeksforgeeks,
+                link: "https://auth.geeksforgeeks.org/user/munotkgm/practice",
+            },
+            {
+                name: "Leetcode",
+                description: "DSA Programming",
+                Icon: SiLeetcode,
+                link: "https://leetcode.com/user4382M/",
+            },
+            {
+                name: "Hacker Earth",
+                description: "Competitive Programming",
+                Icon: SiHackerearth,
+                link: "https://www.hackerearth.com/@munot.kgm",
+            },
+            /* .............Other......... */
+        ],
+    },
+};
+
+export default utilities;
